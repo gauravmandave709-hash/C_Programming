c37f1459edfd8ffde7dd35811fb48cea8156669a
Gaurav Mandave <gauravmandave709@gmail.com>
2026-08-09 10:15:19 +0530

    Add files via upload

---
+++ Assignment 3/07_Print_The_Letter_From_w_To_e_Using_A_Loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<Conio.h>
+
+int main()
+{
+    char Ch = 'w';
+
+    while(Ch >= 'e')
+    {
+        printf("\n %c",Ch);
+        Ch--;
+    }
+    printf("\n \n Closed ");
+
+    return 0;
+}
