59d862d94bfb556379b3c9359c2148f05c1c0589
Gaurav Mandave <gauravmandave709@gmail.com>
2026-08-10 20:49:39 +0530

    Add files via upload

---
+++ Assignment 3/06_Print_The_Letter_From_H_To_N_Using_A_Loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<Conio.h>
+
+int main()
+{
+    char Ch = 'H';
+
+    while(Ch <= 'N')
+    {
+        printf("\n %c",Ch);
+        Ch++;
+    }
+    printf("\n \n Closed ");
+
+    return 0;
+}
